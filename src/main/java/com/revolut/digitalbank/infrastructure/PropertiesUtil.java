cc98f1108a0a0299cd16fa795db80a39c5cd989e
lucasyoshioka <lucasyoshioka1@gmail.com>
2019-01-30 21:53:58 -0200

    Remove jetty properties

--- src/main/java/com/revolut/digitalbank/infrastructure/PropertiesUtil.java
+++ src/main/java/com/revolut/digitalbank/infrastructure/PropertiesUtil.java
@@ -7,15 +7,6 @@ import java.util.Properties;
 
 public class PropertiesUtil {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println(PropertiesUtil.get("jetty.jersey.packages"));
-        System.out.println(PropertiesUtil.get("jetty.application.package"));
-        System.out.println(Integer.valueOf(PropertiesUtil.get("jetty.port")));
-        System.out.println(PropertiesUtil.get("jetty.context.path"));
-        System.out.println(PropertiesUtil.get("jetty.path.spec"));
-        System.out.println(Integer.valueOf(PropertiesUtil.get("jetty.init.order")));
-    }
-
     public static String get(String key) throws IOException {
         String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         String propertiesPath = rootPath + "application.properties";
